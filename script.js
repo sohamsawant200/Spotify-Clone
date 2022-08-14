601d6f091e1f410e062c77b528f20273b874184b
sohamsawant200 <soham.sawant200@gmail.com>
2022-08-14 12:05:30 +0530

    Play and Pause updated with other changes

--- script.js
+++ script.js
@@ -1,58 +1,100 @@
-console.log("Welcome to Spotify")
+console.log("Welcome to Spotify");
 
 // Initializing Parameters
 let songIndex = 0;
-let audioElement = new Audio('/songs/5.mp3');
-let masterPlay = document.getElementById('masterPlay');
+let counter = 0;
+let audioElement = new Audio("/songs/5.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let masterPause = document.getElementById("masterPause");
 // audioElement.play();
-let progressBar = document.getElementById('progressBar'); 
-let playBtn = document.getElementsByClassName('playBtn');
+let progressBar = document.getElementById("progressBar");
+let playbtn = document.getElementById("play-btn");
+let songPlay = document.getElementsByClassName("songPlay");
+let pausebtn = document.getElementById('pause-btn');
+let songItems = Array.from(document.getElementsByClassName("songItem"));
 
 let songs = [
-    {
-        songName: 'Heros-Tonight' , songPath:'/songs/5.mp3' , coverPath:'/covers/5.jpg' 
-    },
-    {
-        songName: 'Heros-Tonight' , songPath:'/songs/5.mp3' , coverPath:'/covers/5.jpg' 
-    },
-    {
-        songName: 'Heros-Tonight' , songPath:'/songs/5.mp3' , coverPath:'/covers/5.jpg' 
-    },
-    {
-        songName: 'Heros-Tonight' , songPath:'/songs/5.mp3' , coverPath:'/covers/5.jpg' 
-    },
-    {
-        songName: 'Heros-Tonight' , songPath:'/songs/5.mp3' , coverPath:'/covers/5.jpg' 
-    }
-    
-]
+  {
+    songName: "Heros Tonight",
+    songPath: "/songs/5.mp3",
+    coverPath: "/covers/5.jpg",
+  },
+  {
+    songName: "Cielo - Huma",
+    songPath: "/songs/2.mp3",
+    coverPath: "/covers/1.jpg",
+  },
+  {
+    songName: "Mortals - Warriyo",
+    songPath: "/songs/1.mp3",
+    coverPath: "/covers/6.jpg",
+  },
+  {
+    songName: "Electronic Beats",
+    songPath: "/songs/3.mp3",
+    coverPath: "/covers/3.jpg",
+  },
+  {
+    songName: "Jazz Music",
+    songPath: "/songs/4.mp3",
+    coverPath: "/covers/9.jpg",
+  },
+];
+
+songItems.forEach((element, i) => {
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
 
 // Play and Pause Event from Masterplay
-masterPlay.addEventListener('click', function(){
-    if(audioElement.paused || audioElement.currrentTime<=0){
-        audioElement.play();
-        console.log('Audio Played');
-    }
-    else{
-        audioElement.pause();
-        console.log('Audio Paused');
-    }
-})
+// masterPlay.addEventListener("click", function () {
+//   if (audioElement.paused || audioElement.currrentTime <= 0) {
+//     audioElement.play();
+//   }
+// });
 
-//Change the Play svg to Pause ***Pending***
-function changeSvg(event){
-    if(event.target.tagName==='masterPlay'){
+// masterPause.addEventListener("click", () => {
+//   audioElement.pause();
+// });
 
-    }
+// Play and Pause Event from Masterplay (Simultaenous Swapping of Play and Pause svgs alongwith Playing and Pausing Audio)
+const toggle = () => {
+  counter = counter + 1;
+  if (counter % 2 !== 0) {
+    masterPlay.classList.add("none");
+    masterPause.classList.remove("none");
+    audioElement.play();
+  } else {
+    masterPause.classList.add("none");
+    masterPlay.classList.remove("none");
+    audioElement.pause();
+  }
 }
+masterPlay.addEventListener("click", toggle);
+masterPause.addEventListener("click", toggle);
+
 
 // Time Update Event in Progress Bar
-audioElement.addEventListener('timeupdate',function(){
-    progress = parseInt(audioElement.currentTime * 100/ audioElement.duration);
-    progressBar.value = progress;
-})
+audioElement.addEventListener("timeupdate", function () {
+  progress = parseInt((audioElement.currentTime * 100) / audioElement.duration);
+  progressBar.value = progress;
+});
 
-// Chnage is Time Event in Progress Bar
-progressBar.addEventListener('change', function(){
-    audioElement.currentTime = (progressBar.value * audioElement.duration / 100);
-})
+// Change in Time Event in Progress Bar
+progressBar.addEventListener("change", function () {
+  audioElement.currentTime = (progressBar.value * audioElement.duration) / 100;
+});
+
+//Playing Audio through PlayBTN
+Array.from(songPlay).forEach( (element) =>{
+  element.addEventListener("click", (e)=> {
+    console.log(e.target);
+    e.target.classList.add("item-none");
+    pausebtn.classList.remove("item-none");
+    // audioElement.play();
+  });
+});
+
+const makeAllPlays = () =>{
+
+}
