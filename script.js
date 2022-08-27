b87d56cdcbf9a324955b79d34b6beaa763bb670e
sohamsawant200 <soham.sawant200@gmail.com>
2022-08-27 11:47:24 +0530

    Completed Project

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ console.log("Welcome to Spotify");
 // Initializing Parameters
 let songIndex = 0;
 let counter = 0;
-let audioElement = new Audio("/songs/5.mp3");
+let audioElement = new Audio("/songs/1.mp3");
 let masterPlay = document.getElementById("masterPlay");
 let masterPause = document.getElementById("masterPause");
+let forward = document.getElementById("forward");
+let backward = document.getElementById("backward");
 // audioElement.play();
 let progressBar = document.getElementById("progressBar");
-let playbtn = document.getElementById("play-btn");
+let playbtn = document.getElementsByClassName("play-btn");
 let songPlay = document.getElementsByClassName("songPlay");
-let pausebtn = document.getElementById('pause-btn');
+let songPause = document.getElementsByClassName("songPause");
+let pausebtn = document.getElementsByClassName("pause-btn");
+let songName = document.getElementById("songName");
+let gif = document.getElementById("gif");
 let songItems = Array.from(document.getElementsByClassName("songItem"));
 
 let songs = [
@@ -64,15 +69,16 @@ const toggle = () => {
     masterPlay.classList.add("none");
     masterPause.classList.remove("none");
     audioElement.play();
+    gif.style.opacity = 1;
   } else {
     masterPause.classList.add("none");
     masterPlay.classList.remove("none");
     audioElement.pause();
+    gif.style.opacity = 0;
   }
-}
+};
 masterPlay.addEventListener("click", toggle);
 masterPause.addEventListener("click", toggle);
-
 
 // Time Update Event in Progress Bar
 audioElement.addEventListener("timeupdate", function () {
@@ -85,16 +91,74 @@ progressBar.addEventListener("change", function () {
   audioElement.currentTime = (progressBar.value * audioElement.duration) / 100;
 });
 
+const makeAllPlays = () => {
+  Array.from(playbtn).forEach((element) => {
+    element.classList.remove("item-none");
+  });
+};
+
+const makeNonePause = () => {
+  Array.from(pausebtn).forEach((element) => {
+    element.classList.add("item-none");
+  });
+};
+
 //Playing Audio through PlayBTN
-Array.from(songPlay).forEach( (element) =>{
-  element.addEventListener("click", (e)=> {
-    console.log(e.target);
-    e.target.classList.add("item-none");
-    pausebtn.classList.remove("item-none");
-    // audioElement.play();
+Array.from(playbtn).forEach((element) => {
+  element.addEventListener("click", (e) => {
+    makeAllPlays();
+    makeNonePause();
+    let i = parseInt(e.target.id) - 1;
+    playbtn[i].classList.add("item-none");
+    pausebtn[i].classList.remove("item-none");
+    // audioElement = new Audio("/songs/[i].mp3");
+    audioElement.src = `songs/${i + 1}.mp3`;
+    console.log(audioElement);
+    songName.innerText = songs[i].songName;
+    audioElement.play();
+    masterPlay.classList.add("none");
+    masterPause.classList.remove("none");
+    gif.style.opacity = 1;
   });
 });
 
-const makeAllPlays = () =>{
+Array.from(pausebtn).forEach((element) => {
+  element.addEventListener("click", (e) => {
+    let i = parseInt(e.target.id) - 1;
+    pausebtn[i].classList.add("item-none");
+    playbtn[i].classList.remove("item-none");
+    audioElement.pause();
+    masterPause.classList.add("none");
+    masterPlay.classList.remove("none");
+    gif.style.opacity = 0;
+  });
+});
 
-}
+// Backward and Forward button functions
+backward.addEventListener("click", () => {
+  if (songIndex <= 0) {
+    songIndex = 4;
+  } else {
+    songIndex -= 1;
+  }
+  audioElement.src = `/songs/${songIndex + 1}.mp3`;
+  songName.innerText = songs[songIndex].songName;
+  audioElement.play();
+  masterPlay.classList.add("none");
+  masterPause.classList.remove("none");
+  gif.style.opacity = 1;
+});
+
+forward.addEventListener("click", () => {
+  if (songIndex >= 4) {
+    songIndex = 0;
+  } else {
+    songIndex += 1;
+  }
+  audioElement.src = `/songs/${songIndex + 1}.mp3`;
+  songName.innerText = songs[songIndex].songName;
+  audioElement.play();
+  masterPlay.classList.add("none");
+  masterPause.classList.remove("none");
+  gif.style.opacity = 1;
+});
